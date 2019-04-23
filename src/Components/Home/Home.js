8149b0cba991509b2811b25c86c18b875b10b806
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-04-23 16:59:41 -0400

    initial commit

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Home extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>API-II-Frontend</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
